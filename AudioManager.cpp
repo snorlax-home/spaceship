f28218b0e75c7f0909c553cb8bcd6d302b521a58
tjong1 <47622135+tjong1@users.noreply.github.com>
2022-09-18 21:54:24 +0800

    Added comments to audio managers

--- AudioManager.cpp
+++ AudioManager.cpp
@@ -3,24 +3,39 @@
 
 using namespace std;
 
+// Constructor and Destructor
+AudioManager::AudioManager()
+{
+}
+
+AudioManager::~AudioManager()
+{
+}
+
 void AudioManager::InitializeAudio()
 {
+	// Create the audio system
 	ResultCheck(FMOD::System_Create(&audioSystem), "FMOD::System_Create Failed");
+	// Initialize the audio system
 	ResultCheck(audioSystem->init(32, FMOD_INIT_NORMAL, extraDriverData), "audioSystem->init Failed");
 }
 
+// Method to get FMOD results and error message for display
 void AudioManager::ResultCheck(FMOD_RESULT result, const char *message)
 {
+	// Check if the result is an error
 	if (result != FMOD_OK)
 	{
 		cout << message << endl;
 	}
 }
 
+// Abstract method to load sounds
 void AudioManager::LoadSounds()
 {
 }
 
+// Getter
 FMOD::System *AudioManager::GetAudioSystem()
 {
 	return audioSystem;
@@ -41,15 +56,8 @@ FMOD_RESULT AudioManager::GetResult()
 	return result;
 }
 
+// Method to update the audio system
 void AudioManager::UpdateSound()
 {
 	ResultCheck(GetAudioSystem()->update(), "Error updating sounds");
-}
-
-AudioManager::AudioManager()
-{
-}
-
-AudioManager::~AudioManager()
-{
 }
--- SpaceshipLevelAudioManager.cpp
+++ SpaceshipLevelAudioManager.cpp
@@ -1,5 +1,6 @@
 #include "SpaceshipLevelAudioManager.h"
 
+// Constructor and Destructor
 SpaceshipLevelAudioManager::SpaceshipLevelAudioManager()
 {
 }
@@ -8,27 +9,39 @@ SpaceshipLevelAudioManager::~SpaceshipLevelAudioManager()
 {
 }
 
+// Method to initialize the audio system
 void SpaceshipLevelAudioManager::InitializeAudio()
 {
+    // Initialize the audio system
     AudioManager::InitializeAudio();
 }
 
+// Method to load sounds
 void SpaceshipLevelAudioManager::LoadSounds()
 {
+    // Create background music
     ResultCheck(GetAudioSystem()->createStream("Assets/Audio/space-theme.wav", FMOD_DEFAULT, 0, &backgroundMusic), "Error creating background music stream");
+    // Set the background music to loop
     ResultCheck(backgroundMusic->setMode(FMOD_LOOP_NORMAL), "Error setting background music mode");
 }
 
+// Method to play background music
 void SpaceshipLevelAudioManager::PlayBackgroundMusic()
 {
+    // Play the background music
     ResultCheck(GetAudioSystem()->playSound(backgroundMusic, 0, false, GetChannelAddress()), "Error playing background music");
 
+    // Set the volume of the background music
     ResultCheck(GetChannel()->setVolume(0.5), "Error setting volume");
+    // Set the pitch of the background music
     ResultCheck(GetChannel()->setPitch(0.5), "Error setting pitch");
 }
 
+// Method to update the audio system and pitch of the background music
 void SpaceshipLevelAudioManager::UpdateSound(int point)
 {
+    // Increase the pitch of the background music based on the point
     ResultCheck(GetChannel()->setPitch(0.5 + (point * 0.25)), "Error setting pitch");
+    // Update the audio system
     AudioManager::UpdateSound();
 }
