b18fa907c4d7df2a592a8541e2855bc4fb9c2644
tjong1 <47622135+tjong1@users.noreply.github.com>
2022-09-25 14:08:49 +0800

    comment collision response for spaceship with spaceship collision

--- Spaceship.cpp
+++ Spaceship.cpp
@@ -356,61 +356,116 @@ void Spaceship::CollisionSpaceship(Spaceship* anotherSpaceship)
     if (CircleCollisionDetection(anotherSpaceship->GetSpriteWidth() / 2,
                                  anotherSpaceship->GetPosition() + anotherSpaceship->GetSpriteCenter()))
     {
-        //https://youtu.be/LPzyNOHY3A4
-        // Push spaceship out of each other
-        D3DXVECTOR2 distance = (GameObject::GetPosition() + GameObject::GetSpriteCenter()) - (anotherSpaceship->GetPosition() + anotherSpaceship->GetSpriteCenter());
-        
+        //------------ Push spaceship out of each other---------------
+        /*
+         * Getting both spaceships center point (vector of the window)
+         * by adding the position of the spaceship (top left of the sprite) and the center of the sprite (vector of the sprite, not window)
+         */
+        D3DXVECTOR2 centerPointPosition = GameObject::GetPosition() + GameObject::GetSpriteCenter();
+        D3DXVECTOR2 anotherSpaceshipCenterPointPosition = anotherSpaceship->GetPosition() + anotherSpaceship->GetSpriteCenter();
+
+        // Get the distance between the two spaceship center points
+        D3DXVECTOR2 distance = centerPointPosition - anotherSpaceshipCenterPointPosition;
+
+        /*
+         * Get the overlap between the two spaceships by using the distance between the two center points and subtracting the radius of the two spaceships
+         * then multiply by 0.5 to get the overlap for each spaceship
+         */
         float overlap = 0.5f * (D3DXVec2Length(&distance) - (GameObject::GetSpriteWidth() / 2) - (anotherSpaceship->GetSpriteWidth() / 2));
-        
+
+        /*
+         * Now, that we know how much they overlap, we need to translate that into a vector
+         * We do this by multiplying overlap with the unit vector of the distance between the two center points
+         * How to get unit vector of distance
+         *  Example: Getting the x component of distance: distance.x / D3DXVec2Length(&distance)
+         * Then we will get the current position and minus the overlap vector to get the new position
+         * minus because we want to push this spaceship in the opposite direction of the other spaceship
+         * do this for x and y component
+         */
         int positionX = GetPosition().x - (overlap * distance.x / D3DXVec2Length(&distance));
         int positionY = GetPosition().y - (overlap * distance.y / D3DXVec2Length(&distance));
-
-        SetPosition(positionX, positionY);
-
+        
+        // Similar to above, but add the overlap vector because we want to push the other spaceship in the opposite direction of this spaceship
         int anotherSpaceshipPositionX = anotherSpaceship->GetPosition().x + (overlap * distance.x / D3DXVec2Length(&distance));
         int anotherSpaceshipPositionY = anotherSpaceship->GetPosition().y + (overlap * distance.y / D3DXVec2Length(&distance));
 
+        // Set the new position of the two spaceships
+        SetPosition(positionX, positionY);
         anotherSpaceship->SetPosition(anotherSpaceshipPositionX, anotherSpaceshipPositionY);
-        
-        // TODO: how to change this to be more realistic?
-        // velocity.x *= -1.2;
-        // velocity.y *= -1.2;
+        //------------ Push spaceship out of each other END---------------
 
-        // anotherSpaceship->SetVelocityX(-velocity.x);
-        // anotherSpaceship->SetVelocityY(-velocity.y);
+        //------------ Calculate new velocity for both spaceships---------------
+        /*
+         * Getting the vector between the two center points and normalize it by dividing the length of the vector
+         * Therefore, we will get the normal unit vector
+         */
+        float normalX = (anotherSpaceshipCenterPointPosition.x - centerPointPosition.x) / D3DXVec2Length(&distance);
+        float normalY = (anotherSpaceshipCenterPointPosition.y - centerPointPosition.y) / D3DXVec2Length(&distance);
 
-        // Normal
-        float normalX = (anotherSpaceship->GetPosition().x - GetPosition().x) / D3DXVec2Length(&distance);
-        float normalY = (anotherSpaceship->GetPosition().y - GetPosition().y) / D3DXVec2Length(&distance);
-
-        // Tangent
+        /*
+         * Get the tangential unit vector by flipping the y-axis and the x-axis of the normal unit vector
+         * Tangential unit vector is perpendicular to the normal unit vector, can be proven using dot product of tangent unit vector and normal unit vector
+         */
         float tangentX = -normalY;
         float tangentY = normalX;
 
-        // Dot Product Tangent
+        /*
+         * Dot Product between the spaceship's velocity vector and the tangential unit vector
+         * i.e. how much of the velocity vector will be transferred towards the tangential vector
+         */
         float dpTangent1 = GetVelocity().x * tangentX + GetVelocity().y * tangentY;
         float dpTangent2 = anotherSpaceship->GetVelocity().x * tangentX + anotherSpaceship->GetVelocity().y * tangentY;
 
-        // Dot Product Normal
+        
+        /*
+         * Dot Product between the spaceship's velocity vector and the normal unit vector
+         * i.e. How much of the velocity vector will be transferred towards the normal vector
+         */
         float dpNormal1 = GetVelocity().x * normalX + GetVelocity().y * normalY;
         float dpNormal2 = anotherSpaceship->GetVelocity().x * normalX + anotherSpaceship->GetVelocity().y * normalY;
-
-        // Conservation of momentum: Elastic Collision
-        float m1 = (dpNormal1 * (GetMass() - anotherSpaceship->GetMass()) + 2.0f * anotherSpaceship->GetMass() * dpNormal2) / (GetMass() + anotherSpaceship->GetMass());
-        float m2 = (dpNormal2 * (anotherSpaceship->GetMass() - GetMass()) + 2.0f * GetMass() * dpNormal1) / (GetMass() + anotherSpaceship->GetMass()); 
-
-
-        float velocityX = tangentX * dpTangent1 + normalX * m1;
-        float velocityY = tangentY * dpTangent1 + normalY * m1;
         
-        float anotherSpaceshipVelocityX = tangentX * dpTangent2 + normalX * m2;
-        float anotherSpaceshipVelocityY = tangentY * dpTangent2 + normalY * m2;
+        /*
+         * One Dimensional Elastic Collision Formula, for the normal
+         * Using the equation
+         * v1 = m1 - m2 / m1 + m2 * v1 + 2 * m2 / m1 + m2 * v2
+         * v2 = m2 - m1 / m1 + m2 * v2 + 2 * m1 / m1 + m2 * v1
+         * u1 is substituted with dpNormal1 
+         * u2 is substituted with dpNormal2
+         * used the velocity transferred to the normal vector instead of directly using the velocity
+         *
+         * Note:
+         * m1 and m2 are the mass of the two objects
+         * u1 and u2 are the initial velocity of the two objects
+         * v1 and v2 are the final velocity of the two objects
+         */
+        
+        float v1 = (GetMass() - anotherSpaceship->GetMass()) / (GetMass() + anotherSpaceship->GetMass()) * dpNormal1 + 2.0f * anotherSpaceship->GetMass() / (GetMass() + anotherSpaceship->GetMass()) * dpNormal2;
+        float v2 = (anotherSpaceship->GetMass() - GetMass()) / (GetMass() + anotherSpaceship->GetMass()) * dpNormal2 + 2.0f * GetMass() / (GetMass() + anotherSpaceship->GetMass()) * dpNormal1;
 
+        /*
+         * Updating this spaceship velocity by adding together the tangential response and the normal response
+         * Tangential response is the tangent unit vector multiply by the velocity transferred to the tangential vector
+         * Tangential response does not need to go through the elastic collision formula because it is not the point of collision
+         * Normal response is the normal unit vector multiply by the final velocity of the normal vector
+         */
+        float velocityX = tangentX * dpTangent1 + normalX * v1;
+        float velocityY = tangentY * dpTangent1 + normalY * v1;
+
+        // Do the same thing for the other spaceship
+        float anotherSpaceshipVelocityX = tangentX * dpTangent2 + normalX * v2;
+        float anotherSpaceshipVelocityY = tangentY * dpTangent2 + normalY * v2;
+
+        // Note: By adding together the velocity vector of this spaceship and the other spaceship,
+        // it should be equal to the velocity of this spaceship before collision response is calculated
+
+        // Set the velocity of this spaceship and the other spaceship
         SetVelocity(velocityX,velocityY);
         anotherSpaceship->SetVelocity(anotherSpaceshipVelocityX, anotherSpaceshipVelocityY);
+        //------------ Calculate new velocity for both spaceships END ---------------
         
         cout << "Collision detected between spaceships" << endl;
-        
+ 
+        // Set bounceSound to true so that the bounce sound will be played
         bounceSound->SetPlaySoundFlag(true);
     }
 }
