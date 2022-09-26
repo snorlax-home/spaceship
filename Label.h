dde27a29ad2f5c59eb3cdba978b1a6b2d8920c31
tyh <tanyihong10@gmail.com>
2022-09-26 08:07:23 +0800

    Comment label and direct 3d manager

--- Label.h
+++ Label.h
@@ -3,6 +3,11 @@
 #include <d3dx9.h>
 #include <string>
 
+/**
+ * \brief UI Component Label
+ *
+ * This UI Label si use for display a text
+ */
 class Label
 {
 private:
@@ -15,12 +20,52 @@ private:
     float width;
     float height;
 public:
-    Label(LPDIRECT3DDEVICE9 d3dDevice, std::string labelText, D3DCOLOR color, D3DXVECTOR2 labelPosition, float width, float height, DWORD format);
+    /**
+     * \brief Label Default Constructor
+     *
+     * \param d3dDevice D3D Device to create font 
+     * \param labelText Text to be display
+     * \param color Color of the label text
+     * \param labelPosition Position of label
+     * \param width Width of the label
+     * \param height Height of the label
+     * \param format Format of the label
+     */
+    Label(LPDIRECT3DDEVICE9 d3dDevice, std::string labelText, D3DCOLOR color, D3DXVECTOR2 labelPosition, float width,
+          float height, DWORD format);
+    /**
+     * \brief Calculate Position Rectangle
+     *
+     * Function to recalculate the position rectangle of the button
+     */
     void CalcRect();
+    /**
+     * \brief Set the label text 
+     * \param labelText String to be display
+     */
     void SetLabelText(std::string labelText);
+    /**
+     * \brief Set position of label
+     * \param labelPosition Position of label
+     */
     void SetLabelPosition(D3DXVECTOR2 labelPosition);
+    /**
+     * \brief Set color of label text
+     * \param color Color of the label
+     */
     void SetColor(D3DCOLOR color);
+    /**
+     * \brief Get position of label
+     * \return Vector of position of label
+     */
     D3DXVECTOR2 GetLabelPosition();
+    /**
+     * \brief Render the label
+     * \param spriteBrush Sprite brush to draw out the label
+     */
     void Render(LPD3DXSPRITE spriteBrush);
+    /**
+     * \brief Clean Up
+     */
     void CleanUp();
 };
